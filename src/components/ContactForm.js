92329b40506f4ff5ec5e57c3f06d636046b46734
Brandon T. Bande <brandonbande01@gmail.com>
2023-03-29 05:40:52 +0200

    Footer added

---
+++ src/components/ContactForm.js
@@ -0,0 +1,66 @@
+import React , {useState} from "react"
+
+const ContactForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [errors, setErrors] = useState({});
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+  
+      // Validate form fields
+      let formErrors = {};
+      if (!name) formErrors.name = 'Name is required';
+      if (!email) formErrors.email = 'Email is required';
+      if (!message) formErrors.message = 'Message is required';
+      setErrors(formErrors);
+  
+      // Only submit form if there are no errors
+      if (Object.keys(formErrors).length === 0) {
+        // Open email app with pre-filled information
+        window.open(
+          `mailto:brandonbande@gmail.com?subject=Contact from ${name}&body=${message}`
+        );
+  
+        // Clear form fields
+        setName('');
+        setEmail('');
+        setMessage('');
+      }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} style={{ width: '200px' }}>
+          <input
+            type="text"
+            id="name"
+            placeholder = 'Enter your name'
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          {errors.name && <div>{errors.name}</div>}
+          <br />
+          <input
+            type="email"
+            id="email"
+            placeholder = 'Enter your email'
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          {errors.email && <div>{errors.email}</div>}
+          <br />
+          <textarea
+            id="message"
+            value={message}
+            placeholder = 'Enter your message'
+            onChange={(event) => setMessage(event.target.value)}
+          />
+          {errors.message && <div>{errors.message}</div>}
+          <br />
+          <button type="submit" className="formSubmit">Submit</button>
+        </form>
+      );
+    };
+
+    export default ContactForm;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,33 @@
-//import { Facebook, LinkedIn, Twitter } from '@mui/icons-material'
-import { Facebook, LinkedIn, Mail, Twitter, YouTube } from '@mui/icons-material';
+
+import { Facebook, LinkedIn, Mail, Twitter} from '@mui/icons-material';
 import React from 'react'
-import Accordion from 'react-bootstrap/Accordion';
+import ContactForm from './ContactForm';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 function Footer() {
   return (
     <div className='footer'>
        <div className='footerMain'>
-       <div className='socialMedia'>
-        <Accordion>
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
-            <Accordion.Body>
-              This is the first item's accordion body.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
-            <Accordion.Body>
-              This is the second item's accordion body.
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
-        </div>
         <div className='socialMedia'>
-            <h2>Links</h2>
-            <Facebook />
-            <LinkedIn />
-            <Twitter />
-            <Mail />
-            <YouTube />
-        </div>
-        <div className='socialMedia'>
+  <h2>Contact Us</h2>
+      <a href="http://www.facebook.com/brandonbande">
+        <Facebook />
+      </a>
+      <a href="https://www.linkedin.com/in/brandoebande">
+        <LinkedIn />
+      </a>
+      <a href="https://www.twitter.com/BandeBrandon">
+        <Twitter />
+      </a>
+      <a href="mailto:brandonbande01@gmail.com">
+        <Mail />
+      </a>
+    </div>
+        <div className='contactForm'>
             Details
-            <p>Hi</p>
-            <p>icon2</p>
+            <ContactForm />
         </div>
        </div>
         <p className='footerSig'>RevisionPal &copy; 2023</p>
